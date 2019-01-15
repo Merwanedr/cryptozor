d633af4f46c2642a995f6160f2774894ebb61de8
Merwane Draï <merwanedr@gmail.com>
2019-01-15 23:48:01 +0100

    changed setup.py

--- cryptozor/__init__.py
+++
@@ -1,2 +0,0 @@
-name = "cryptozor"
-import cryptozor
--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="cryptozor.py",
-    version="1.0.2",
+    version="1.0.3",
     author="Merwane Draï",
     author_email="merwanedr@gmail.com",
     description="A Python Cryptocurrency converter.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/merwanedr/cryptozor",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    url="https://github.com/merwanedr/cryptozor.py",
+    py_modules=["cryptozor"],
+    package_dir={'': 'src'},
 )
