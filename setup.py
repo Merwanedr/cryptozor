cb4ea75a56b7e2caa8f90df96d6f2eef46f6323a
Merwane Draï <merwanedr@gmail.com>
2019-01-15 22:22:06 +0100

    Restructured folders
    This is my first attempt to create a PyPi package I am struggling ...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cryptozor",
-    version="1.0.1",
+    version="1.0.2",
     author="Merwane Draï",
     author_email="merwanedr@gmail.com",
     description="A Python Cryptocurrency converter.",
