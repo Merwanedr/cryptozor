705a7fc37c54181345d056924157b794c00dc297
Merwane Draï <merwanedr@gmail.com>
2019-01-15 21:45:30 +0100

    fixed __init__.py file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cryptozor",
-    version="1.0.0",
+    version="1.0.1",
     author="Merwane Draï",
     author_email="merwanedr@gmail.com",
     description="A Python Cryptocurrency converter.",
